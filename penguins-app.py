af2dadf61abb403a23d155d891ef9622c41e54d9
khw11044 <khw11044@gmail.com>
2021-01-21 01:21:21 +0900

    sd

--- penguins-app.py
+++ penguins-app.py
@@ -3,78 +3,42 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+from cryptocmd import CmcScraper
+import pandas_datareader as pdr 
+import plotly.express as px
+from datetime import datetime, timedelta
 
-st.write("""
-# Penguin Prediction App
+st.write('# 비트코인 데이터 Web app')
 
-This app predicts the **Palmer Penguin** species!
+st.sidebar.header('Menu')
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
-""")
+name = st.sidebar.selectbox('Name', ['BTC', 'ETH', 'USDT'])
 
-st.sidebar.header('User Input Features')
+sevendayago = datetime.today() - timedelta(7)
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+start_date = st.sidebar.date_input('Start date', sevendayago)
+end_date = st.sidebar.date_input('End date', datetime.today())
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+# https://coinmarketcap.com
+scraper = CmcScraper(name, start_date.strftime('%d-%m-%Y'), end_date.strftime('%d-%m-%Y'))
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
+df = scraper.get_dataframe()
 
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
+fig_close = px.line(df, x='Date', y=['Open', 'High', 'Low', 'Close'], title='가격')
+fig_volume = px.line(df, x='Date', y=['Volume'], title='Volume')
 
-# Displays the user input features
-st.subheader('User Input features')
-
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
-
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
-
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+st.plotly_chart(fig_close)
+st.plotly_chart(fig_volume)
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+st.write('''
+# 삼성전자 주식 데이터
+마감 가격과 거래량을 차트로 보여줍니다!
+''')
+
+# https://finance.yahoo.com/quote/005930.KS?p=005930.KS
+dr = pdr.get_data_yahoo('005930.KS',start_date,end_date)
+
+st.line_chart(dr.Close)
+st.line_chart(dr.Volume)
